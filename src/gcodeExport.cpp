ff35dce616f861a5d87e21ed3a2c9a8399e51b17
Christopher Jung <christopher.h.jung@web.de>
2020-08-11 14:11:20 +0200

    support format replacement

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -5,6 +5,10 @@
 #include <cmath>
 #include <iomanip>
 #include <stdarg.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <regex>
 
 #include "Application.h" //To send layer view data.
 #include "ExtruderTrain.h"
@@ -566,9 +570,55 @@ void GCodeExport::writeLayerCountComment(const size_t layer_count)
     *output_stream << ";LAYER_COUNT:" << layer_count << new_line;
 }
 
+
+template<class BidirIt, class Traits, class CharT, class UnaryFunction>
+std::basic_string<CharT> regex_replace(BidirIt first, BidirIt last, const std::basic_regex<CharT,Traits>& re, UnaryFunction f)
+{
+    std::basic_string<CharT> s;
+
+    typename std::match_results<BidirIt>::difference_type
+            positionOfLastMatch = 0;
+    auto endOfLastMatch = first;
+
+    auto callback = [&](const std::match_results<BidirIt>& match)
+    {
+        auto positionOfThisMatch = match.position(0);
+        auto diff = positionOfThisMatch - positionOfLastMatch;
+
+        auto startOfThisMatch = endOfLastMatch;
+        std::advance(startOfThisMatch, diff);
+
+        s.append(endOfLastMatch, startOfThisMatch);
+        s.append(f(match));
+
+        auto lengthOfMatch = match.length(0);
+
+        positionOfLastMatch = positionOfThisMatch + lengthOfMatch;
+
+        endOfLastMatch = startOfThisMatch;
+        std::advance(endOfLastMatch, lengthOfMatch);
+    };
+
+    std::regex_iterator<BidirIt> begin(first, last, re), end;
+    std::for_each(begin, end, callback);
+
+    s.append(endOfLastMatch, last);
+
+    return s;
+}
+
+template<class Traits, class CharT, class UnaryFunction>
+std::string regex_replace(const std::string& s,
+                          const std::basic_regex<CharT,Traits>& re, UnaryFunction f)
+{
+    return regex_replace(s.cbegin(), s.cend(), re, f);
+}
+
 void GCodeExport::writeLine(const char* line)
 {
-    *output_stream << line << new_line;
+    *output_stream << regex_replace(line, std::regex("\\{(.+?)\\}"), [&](const std::smatch& m){
+        return Application::getInstance().current_slice->scene.extruders[current_extruder].settings.get<std::string>(m.str(1));
+    }) << new_line;
 }
 
 void GCodeExport::writeExtrusionMode(bool set_relative_extrusion_mode)
